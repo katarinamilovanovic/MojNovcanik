6474460c6ee298b29029a25311972b49de1dcb72
Katarina Milovanovic <katarina.milovanovic@expertgroup.rs>
2024-01-11 17:39:00 +0100

    Komit1

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -23,19 +23,19 @@ export class RegisterPage implements OnInit {
 
  async signup(){ 
     await this.authenticationService.signup({name: this.name, email:this.email,password:this.password}).then(res=>{
-      /*if(res.user.uid){
+      if(res.user.uid){
         let data = {
           name:this.name,
           email:this.email,
           password:this.password,
-          //id:res.user.uid
+          id:res.user.uid
         }
         this.authenticationService.saveDetails(data).then(res=>{
          alert('Account Created!');
         },err=>{
           console.log(err);
         })
-      }*/
+      }
     },err=>{
       alert(err.message);
       console.log(err);
