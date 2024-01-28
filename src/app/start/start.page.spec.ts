982b520aa7257208053a7650fa8e83bb0765ef4a
Katarina <kaca.milosevic001@gmail.com>
2024-01-28 14:22:43 +0100

    Update dugme

--- src/app/start/start.page.spec.ts
+++ src/app/start/start.page.spec.ts
@@ -1,22 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
 import { StartPage } from './start.page';
 
 describe('StartPage', () => {
   let component: StartPage;
   let fixture: ComponentFixture<StartPage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [StartPage],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(StartPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
